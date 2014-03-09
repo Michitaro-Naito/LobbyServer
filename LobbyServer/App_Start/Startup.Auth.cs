0788f912db55a00154583d9b8904f66aa80799c3
Michitaro Naito <m-naito@amlitek.com>
2014-03-09 12:21:09 +0900

    Remove: ASP.NET Default Authentication and Database things.

--- LobbyServer/App_Start/Startup.Auth.cs
+++ LobbyServer/App_Start/Startup.Auth.cs
@@ -10,14 +10,14 @@ namespace LobbyServer
         // 認証設定の詳細については、http://go.microsoft.com/fwlink/?LinkId=301864 を参照してください
         public void ConfigureAuth(IAppBuilder app)
         {
-            // アプリケーションが Cookie を使用して、サインインしたユーザーの情報を格納できるようにします
+            /*// アプリケーションが Cookie を使用して、サインインしたユーザーの情報を格納できるようにします
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
                 LoginPath = new PathString("/Account/Login")
             });
             // Use a cookie to temporarily store information about a user logging in with a third party login provider
-            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);*/
 
             // 次の行のコメントを解除して、サード パーティのログイン プロバイダーを使用したログインを有効にします
             //app.UseMicrosoftAccountAuthentication(
