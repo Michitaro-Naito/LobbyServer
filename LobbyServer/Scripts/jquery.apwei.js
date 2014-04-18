dfa45b775c6b44936146609e99395081e9c75a9f
Michitaro Naito <m-naito@amlitek.com>
2014-04-18 14:03:44 +0900

    Limit PlayLog/Download

--- LobbyServer/Scripts/jquery.apwei.js
+++ LobbyServer/Scripts/jquery.apwei.js
@@ -2,12 +2,27 @@
 
     // Apwei jQuery Extension
 
-    $.fn.disableFor = function (milliseconds) {
+    // Enables
+    $.fn.enable = function () {
+        var s = this;
+        s.removeAttr('disabled');
+        return s;
+    }
+
+    // Disables
+    $.fn.disable = function () {
         var s = this;
         s.attr('disabled', 'disabled');
+        return s;
+    }
+
+    // Disables for a period of time.
+    $.fn.disableFor = function (milliseconds) {
+        var s = this;
+        s.disable();
         setTimeout(function () {
-            s.removeAttr('disabled');
+            s.enable();
         }, milliseconds);
-        return this;
+        return s;
     }
 })();
